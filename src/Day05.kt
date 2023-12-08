35b366724e3df943e35cadf986d9e237c48b004c
Sebastian Schalow <schalowseb@gmail.com>
2023-12-08 19:35:01 +0100

    Solve day5 part1 with range checks instead of putting all possible values in range into a map

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,76 +1,132 @@
+class Mapping(mappingString: String) {
+
+    private var mappingString: String = ""
+    var targetBase: Long = -1
+    var from: Long = -1
+    var step: Long = -1
+    var end: Long = -1
+
+    private fun extractMappingFromMappingString() {
+        val mappingValues = mappingString.split(" ")
+        targetBase = mappingValues[0].toLong()
+        from = mappingValues[1].toLong()
+        step = mappingValues[2].toLong()
+        end = from + step
+    }
+
+    init {
+        this.mappingString = mappingString
+        extractMappingFromMappingString()
+    }
+
+    override fun toString(): String {
+        return "$targetBase $from $step"
+    }
+
+}
+
 class SeedManager(input: List<String>) {
 
     private var input: List<String> = mutableListOf()
     private var seedList: List<Long> = mutableListOf()
 
-    private fun initializeCorrespondingMapFromXToY(x: String, y: String, map: MutableMap<Long, Long>) {
+    private fun initializeXToYMappingFor(x: String, y: String, list: MutableList<Mapping>) {
         val xToYStartIndex = input.indexOf(x) + 1
         val xToYEndIndex = if (y != "") input.indexOf(y) - 2 else input.size - 1
 
-        for (i in xToYStartIndex..xToYEndIndex) {
-            val mappingString = input[i].split(' ')
-            val mapTo = mappingString[0].toLong()
-            val begin = mappingString[1].toLong()
-            val range = mappingString[2].toLong()
-            for (step in 0..range - 1) {
-                if (map.get(begin + step) == null) map.put(begin + step, mapTo + step)
+        for (mappingEntryIndex in xToYStartIndex..xToYEndIndex) {
+            list.add(Mapping(input[mappingEntryIndex]))
+        }
+    }
+
+    private fun getMappingResultFromListFor(toMap: Long, list: MutableList<Mapping>): Long {
+        var mappingResult = toMap
+        for (mapEntry in list) {
+            if (toMap >= mapEntry.from && toMap <= mapEntry.end) {
+                mappingResult = mapEntry.targetBase + (toMap - mapEntry.from)
+                break
             }
         }
+        return mappingResult
     }
 
     private fun extractSeeds(): List<Long> = input[0].split("seeds: ")[1].split(' ').map { it -> it.toLong() }
 
     public fun getSeeds(): List<Long> = seedList
 
-    public fun getLocationFor(seed: Long): Long {
-        val soilForSeed = seedToSoilMap.getOrDefault(seed, seed)
-        val fertilizerForSoil = soilToFertilizerMap.getOrDefault(soilForSeed, soilForSeed)
-        val waterForFertilizer = fertilizerToWaterMap.getOrDefault(fertilizerForSoil, fertilizerForSoil)
-        val lightForWater = waterToLightMap.getOrDefault(waterForFertilizer, waterForFertilizer)
-        val tempForLight = lightToTemperatureMap.getOrDefault(lightForWater, lightForWater)
-        val humidityForTemp = temperatureToHumidityMap.getOrDefault(tempForLight, tempForLight)
-        return humidityToLocationMap.getOrDefault(humidityForTemp, humidityForTemp)
-    }
+    public fun getLocationFor(seed: Long) =
+        getMappingResultFromListFor(
+            getMappingResultFromListFor(
+                getMappingResultFromListFor(
+                    getMappingResultFromListFor(
+                        getMappingResultFromListFor(
+                            getMappingResultFromListFor(
+                                getMappingResultFromListFor(
+                                    seed,
+                                    seedToSoilMappingsList
+                                ),
+                                soilToFertilizerMappingsList
+                            ), fertilizerToWaterMappingsList
+                        ), waterToLightMappingsList
+                    ), lightToTempMappingsList
+                ), tempToHumidityMappingsList
+            ), humidityToLocationMappingsList
+        )
 
     init {
         this.input = input
         seedList = extractSeeds()
-        initializeCorrespondingMapFromXToY("seed-to-soil map:", "soil-to-fertilizer map:", seedToSoilMap)
-        initializeCorrespondingMapFromXToY("soil-to-fertilizer map:", "fertilizer-to-water map:", soilToFertilizerMap)
-        initializeCorrespondingMapFromXToY("fertilizer-to-water map:", "water-to-light map:", fertilizerToWaterMap)
-        initializeCorrespondingMapFromXToY("water-to-light map:", "light-to-temperature map:", waterToLightMap)
-        initializeCorrespondingMapFromXToY(
-            "light-to-temperature map:",
-            "temperature-to-humidity map:",
-            lightToTemperatureMap
+
+        initializeXToYMappingFor(
+            "seed-to-soil map:", "soil-to-fertilizer map:",
+            seedToSoilMappingsList
         )
-        initializeCorrespondingMapFromXToY(
-            "temperature-to-humidity map:",
-            "humidity-to-location map:",
-            temperatureToHumidityMap
+        initializeXToYMappingFor(
+            "soil-to-fertilizer map:", "fertilizer-to-water map:",
+            soilToFertilizerMappingsList
         )
-        initializeCorrespondingMapFromXToY("humidity-to-location map:", "", humidityToLocationMap)
+        initializeXToYMappingFor(
+            "fertilizer-to-water map:", "water-to-light map:",
+            fertilizerToWaterMappingsList
+        )
+        initializeXToYMappingFor(
+            "water-to-light map:", "light-to-temperature map:",
+            waterToLightMappingsList
+        )
+        initializeXToYMappingFor(
+            "light-to-temperature map:", "temperature-to-humidity map:",
+            lightToTempMappingsList
+        )
+        initializeXToYMappingFor(
+            "temperature-to-humidity map:", "humidity-to-location map:",
+            tempToHumidityMappingsList
+        )
+        initializeXToYMappingFor(
+            "humidity-to-location map:", "",
+            humidityToLocationMappingsList
+        )
+
         println("initialized all mappings")
     }
 
     companion object {
-        var seedToSoilMap: MutableMap<Long, Long> = mutableMapOf()
-        var soilToFertilizerMap: MutableMap<Long, Long> = mutableMapOf()
-        var fertilizerToWaterMap: MutableMap<Long, Long> = mutableMapOf()
-        var waterToLightMap: MutableMap<Long, Long> = mutableMapOf()
-        var lightToTemperatureMap: MutableMap<Long, Long> = mutableMapOf()
-        var temperatureToHumidityMap: MutableMap<Long, Long> = mutableMapOf()
-        var humidityToLocationMap: MutableMap<Long, Long> = mutableMapOf()
+        val seedToSoilMappingsList: MutableList<Mapping> = mutableListOf()
+        val soilToFertilizerMappingsList: MutableList<Mapping> = mutableListOf()
+        val fertilizerToWaterMappingsList: MutableList<Mapping> = mutableListOf()
+        val waterToLightMappingsList: MutableList<Mapping> = mutableListOf()
+        val lightToTempMappingsList: MutableList<Mapping> = mutableListOf()
+        val tempToHumidityMappingsList: MutableList<Mapping> = mutableListOf()
+        val humidityToLocationMappingsList: MutableList<Mapping> = mutableListOf()
     }
 
 }
 
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
 
         val seedManager = SeedManager(input)
         val seeds = seedManager.getSeeds()
-        var locations = mutableSetOf<Long>()
+        val locations = mutableSetOf<Long>()
 
         println("seeds are $seeds")
 
@@ -78,18 +134,19 @@ fun main() {
             locations.add(seedManager.getLocationFor(seed))
         }
 
-        return locations.min().toInt()
+        println("locations are $locations")
+        return locations.min()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        return input.size.toLong()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 35)
+    // check(part1(testInput) == 35)
 
     val input = readInput("Day05")
     part1(input).println()
-    // part2(input).println()
+    // part2(input).prLongln()
 }
